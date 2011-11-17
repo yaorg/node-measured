e245ca3787eddc3f725a1ad4cb78a6aba953f223
Felix Geisendörfer <felix@debuggable.com>
2011-11-17 14:24:18 +0100

    Implement Stopwatch

---
+++ test/unit/util/test-Stopwatch.js
@@ -0,0 +1,60 @@
+var common    = require('../../common');
+var test      = require('utest');
+var assert    = require('assert');
+var Stopwatch = common.betterMetrics.Stopwatch;
+var sinon     = require('sinon');
+
+var watch;
+var clock;
+test('Stopwatch', {
+  before: function() {
+    clock = sinon.useFakeTimers();
+    watch = new Stopwatch();
+  },
+
+  after: function() {
+    clock.restore();
+  },
+
+  'start and stop': function() {
+    var watch = new Stopwatch();
+    watch.start();
+    clock.tick(100);
+
+    var elapsed = watch.stop();
+    assert.equal(elapsed, 100);
+  },
+
+  'emits time on stop': function() {
+    var watch = new Stopwatch();
+    watch.start();
+    clock.tick(20);
+
+    var time;
+    watch.on('stop', function(_time) {
+      time = _time;
+    });
+
+    watch.stop();
+
+    assert.equal(time, 20);
+  },
+
+  'becomes useless after being stopped once': function() {
+    var watch = new Stopwatch();
+    watch.start();
+    clock.tick(20);
+
+    var time;
+    watch.on('stop', function(_time) {
+      time = _time;
+    });
+
+    assert.equal(watch.stop(), 20);
+    assert.equal(time, 20);
+
+    time = null;
+    assert.equal(watch.stop(), undefined);
+    assert.equal(time, null);
+  },
+});
