7ab7fa17793c31a6dd6d9c64b3469ddd85f21924
Felix Geisendörfer <felix@debuggable.com>
2011-11-17 14:48:40 +0100

    Better collections

--- test/unit/test-Collection.js
+++ test/unit/test-Collection.js
@@ -2,7 +2,12 @@ var common = require('../common');
 var test   = require('utest');
 var assert = require('assert');
 
+var collection;
 test('Collection', {
+  before: function() {
+    collection = new common.betterMetrics.Collection();
+  },
+
   'with two counters': function() {
     var collection = new common.betterMetrics.Collection('counters');
     var a = collection.counter('a');
@@ -19,5 +24,18 @@ test('Collection', {
         count: 5,
       },
     });
+  },
+
+  'returns same metric object when given the same name': function() {
+    var a1 = collection.counter('a');
+    var a2 = collection.counter('a');
+
+    assert.strictEqual(a1, a2);
+  },
+
+  'throws exception when creating a metric without name': function() {
+    assert.throws(function() {
+      collection.counter()
+    }, /Collection.NoMetricName/);
   },
 });
