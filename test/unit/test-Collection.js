02fe512d142a4642db94a9f6b2082e56312b5401
Andrew <email@andrewrea.co.uk>
2016-04-01 10:53:59 +0100

    Putting a test back in which I removed

--- test/unit/test-Collection.js
+++ test/unit/test-Collection.js
@@ -11,6 +11,35 @@ describe('Collection', function () {
     collection = common.measured.createCollection();
   });
 
+  it('with two counters', function () {
+    collection = new common.measured.Collection('counters');
+    var a = collection.counter('a');
+    var b = collection.counter('b');
+
+    a.inc(3);
+    b.inc(5);
+
+    assert.deepEqual(collection.toJSON(), {
+      'counters': {
+        'a': 3,
+        'b': 5
+      }
+    });
+  });
+
+  it('returns same metric object when given the same name', function () {
+    var a1 = collection.counter('a');
+    var a2 = collection.counter('a');
+
+    assert.strictEqual(a1, a2);
+  });
+
+  it('throws exception when creating a metric without name', function () {
+    assert.throws(function () {
+      collection.counter();
+    }, /Collection\.NoMetricName/);
+  });
+
   describe.only('toFlatJSON', function(){
     beforeEach(function(){
       collection = new common.measured.Collection('my-collection');
@@ -113,18 +142,5 @@ describe('Collection', function () {
       assert.ok(result.group,'my-collection');
       assert.ok(result.type,'timer');
     });
-  });
-
-  it('returns same metric object when given the same name', function () {
-    var a1 = collection.counter('a');
-    var a2 = collection.counter('a');
-
-    assert.strictEqual(a1, a2);
-  });
-
-  it('throws exception when creating a metric without name', function () {
-    assert.throws(function () {
-      collection.counter();
-    }, /Collection\.NoMetricName/);
   });
 });
