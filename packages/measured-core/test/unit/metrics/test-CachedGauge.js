849988c01697656556ba0eb89cfe122607196717
Justin Field <fieldju@gmail.com>
2018-06-02 09:29:07 -0700

    Add untracked files

---
+++ packages/measured-core/test/unit/metrics/test-CachedGauge.js
@@ -0,0 +1,55 @@
+/*global describe, it, beforeEach, afterEach*/
+const TimeUnits = require('../../../lib/util/units');
+const CachedGauge = require('../../../lib/metrics/CachedGauge');
+const assert = require('assert');
+
+describe('CachedGauge', () => {
+  let cachedGauge;
+  it('A cachedGauge immediately calls the callback to set its initial value', async () => {
+    cachedGauge = new CachedGauge(
+      () => {
+        return new Promise(resolve => {
+          resolve(10);
+        });
+      },
+      1,
+      TimeUnits.MINUTES
+    ); // Shouldn't update in the unit test.
+
+    await wait(5 * TimeUnits.MILLISECONDS);
+
+    assert.equal(cachedGauge.toJSON(), 10);
+  });
+
+  it('A cachedGauge calls the callback at the interval provided', async () => {
+    const values = [1, 2];
+    cachedGauge = new CachedGauge(
+      () => {
+        return new Promise(resolve => {
+          resolve(values.shift());
+        });
+      },
+      5,
+      TimeUnits.MILLISECONDS
+    );
+
+    await wait(7 * TimeUnits.MILLISECONDS);
+
+    assert.equal(cachedGauge.toJSON(), 2);
+    assert.equal(values.length, 0, 'the callback should have been called 2x, emptying the values array');
+  });
+
+  afterEach(() => {
+    if (cachedGauge) {
+      cachedGauge.end();
+    }
+  });
+});
+
+const wait = waitInterval => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, waitInterval);
+  });
+};
