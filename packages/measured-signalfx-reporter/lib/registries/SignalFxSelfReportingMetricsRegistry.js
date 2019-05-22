be283ac0e07b6cff3491ac0e98b65f606fa8b004
Ana Casner <ana.casner@nike.com>
2019-05-22 12:48:29 -0700

    feat: The sendEvent wrapper now returns a promise.
    In the SignalFx library sendEvent is async and returns a promise. In the
    SignalFxSelfReportingMetricsRegistry, the sendEvent wrapper always
    returned undefined (since it was returning the result of forEach). So
    there was no way to track when it was done.

--- packages/measured-signalfx-reporter/lib/registries/SignalFxSelfReportingMetricsRegistry.js
+++ packages/measured-signalfx-reporter/lib/registries/SignalFxSelfReportingMetricsRegistry.js
@@ -70,11 +70,9 @@ class SignalFxSelfReportingMetricsRegistry extends SelfReportingMetricsRegistry 
    * registry.sendEvent('uncaughtException', SignalFxEventCategories.ALERT);
    */
   sendEvent(eventType, category, dimensions, properties, timestamp) {
-    return this._reporters.forEach(reporter => {
-      if (typeof reporter.sendEvent === 'function') {
-        reporter.sendEvent(eventType, category, dimensions, properties, timestamp);
-      }
-    });
+    return Promise.all(this._reporters
+      .filter(reporter => typeof reporter.sendEvent === 'function')
+      .map(reporter => reporter.sendEvent(eventType, category, dimensions, properties, timestamp)));
   }
 }
 
